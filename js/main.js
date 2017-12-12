ee38e1a12b9742654bf7298aa607b06ff9180d56
Icia_cr <icia83@gmail.com>
2017-12-12 09:55:00 +0100

    ejercicio 2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 'use strict';
 
+var cad = "";
+var phrase = document.getElementById('phraseLoop');
+var select = '<select name="colores"><option value="1">blanco</option><option value="2">azul</option><option value="3">rojo</option><option value="4">verde</option><option value="5">amarillo</option><option value="6">rosa</option></select>'
 function bucle(){
 	for (var i = 0; i < 10; i++) {
-		phrase.innerHTML += "He aprendido bien cómo funcionan los bucles" + colors + "<br>";
-    colors.innerHTML += colorsValue;
-	}
-}
-var phrase = document.getElementById('phraseLoop');
-var colors = document.getElementById('colors').value;
-//var colorsValue = colors.options[colors].value;
+		cad += '<div class="colors">He aprendido bien cómo funcionan los bucles '+ select + '</div>';
 
-console.log(colorsValue);
-bucle();
+   //	colors.innerHTML += colorsValue;
+	}
+	return cad;
+}
+phrase.innerHTML = bucle();
+//var colors = document.getElementById('colors').value;
+//var colorsValue = colors.option[colors].value;
+
+//console.log(colorsValue);
